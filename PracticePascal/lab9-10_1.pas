712764d10dabbd8c92b656aee89b66195485f0e3
hindiluvv <whoisdzheev@gmail.com>
2025-11-18 12:17:28 +0300

    Add files via upload

---
+++ PracticePascal/lab9-10_1.pas
@@ -0,0 +1,64 @@
+﻿procedure InputArray(var a: array of integer);
+begin
+  for var i := 0 to High(a) do
+    Read(a[i]);
+end;
+
+function FirstMultipleOf5(a: array of integer): integer;
+begin
+  for var i := 0 to High(a) do
+    if a[i] mod 5 = 0 then
+      exit(i);
+  Result := 10000; 
+end;
+
+procedure ReplaceMaxWithZero(var a: array of integer);
+var max, index: integer;
+begin
+  max := a[0]; index := 0;
+  for var i := 1 to High(a) do
+    if a[i] > max then
+    begin
+      max := a[i];
+      index := i;
+    end;
+  a[index] := 0;
+end;
+
+procedure DoubleAfterMin(var a: array of integer);
+var min, index: integer;
+begin
+  min := a[0]; index := 0;
+  for var i := 1 to High(a) do
+    if a[i] < min then
+    begin
+      min := a[i];
+      index := i;
+    end;
+  for var i := index+1 to High(a) do
+    a[i] := a[i] * 2;
+end;
+
+begin
+  var n: integer;
+  Readln(n);
+  var a := new integer[n];
+  var b := new integer[n];
+
+  InputArray(a);
+  InputArray(b);
+
+  if FirstMultipleOf5(a) < FirstMultipleOf5(b) then
+  begin
+    ReplaceMaxWithZero(a);
+    DoubleAfterMin(b);
+  end
+  else
+  begin
+    ReplaceMaxWithZero(b);
+    DoubleAfterMin(a);
+  end;
+
+  println('Массив A: ', a);
+  println('Массив B: ', b);
+end.
---
+++ PracticePascal/lab9-10_2.pas
@@ -0,0 +1,21 @@
+﻿begin
+  var a := new integer[6,8];
+  var b := new integer[6];
+
+  for var i := 0 to 5 do
+    for var j := 0 to 7 do
+      Read(a[i,j]);
+
+  for var i := 0 to 5 do
+  begin
+    b[i] := 0;
+    for var j := 0 to 7 do
+      if abs(a[i,j]) mod 4 = 0 then
+      begin
+        b[i] := a[i,j];
+        break;
+      end;
+  end;
+
+  println('Результат: ', b);
+end.
