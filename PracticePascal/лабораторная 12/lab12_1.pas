81e0ced3e41b1513e8f826861d0275bc57b16848
hindiluvv <whoisdzheev@gmail.com>
2025-11-29 09:04:07 +0300

    Add files via upload

---
+++ PracticePascal/лабораторная 12/lab12_1.pas
@@ -0,0 +1,20 @@
+﻿var
+  filetext: text;
+  a: string;
+  i: integer;
+begin
+  assign(filetext,'D:\text.txt');
+  rewrite(filetext);
+  for i:= 1 to 10 do
+    writeln(filetext,i);
+    close(filetext);
+    
+    assign(filetext,'D:\text.txt');
+    reset(filetext);
+  for i:= 1 to 10 do
+    begin
+      readln(filetext,a);
+      writeln(a);
+    end;
+  close(filetext);
+end.
